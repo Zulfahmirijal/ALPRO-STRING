f9b957dbbe84ee6f37ccec97468444434a39230b
Zulfahmirijal <61892937+Zulfahmirijal@users.noreply.github.com>
2020-03-30 19:58:26 +0800

    Add files via upload

---
+++ Alpro(2)_string6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main (){
+	char text1[]="apa";
+	char text2[]="kakak";
+	char text3[]="macam";
+	
+	strrev(text1);
+	cout<<text1<<endl;
+	
+	strrev(text2);
+	cout<<text2<<endl;
+	
+	strrev(text3);
+	cout<<text3<<endl;
+}
